9f94eb7a446ad97bf8b69a39049d1304880356a4
Penkwe Cjengh <498296816a@gmail.com>
2021-10-27 14:42:16 +0800

    6.4 change to non-block mode

--- kmodules/rlk_lab/rlk_basic/chapter_6_device_driver/test.c
+++ kmodules/rlk_lab/rlk_basic/chapter_6_device_driver/test.c
@@ -7,31 +7,38 @@
 
 int main() {
 
-	char message[] = "Testing the virtual FIFO device"; //and more character and more and more than 64";
+	char message[90] = "Testing the virtual FIFO device with length of 80 and more and more and how dare you"; //and more character and more and more than 64";
 	size_t len = sizeof(message);
-
-	int fd = open(DEMO_DEV_NAME, O_RDWR);
-	if (fd < 0) {
-		printf("open devicei %s failed\n", DEMO_DEV_NAME);
-		return -1;
-	}
-
-	int ret = write(fd, message, len);
-	if (ret != len) {
-		printf("len %ld and ret %d dont match on device %d ", len, ret, fd);
-		return -1;
-	}
 
 	char* read_buffer = (char*)malloc(2*len);
 	memset(read_buffer, 0, 2*len);
 
-	//close(fd);
-	
-	fd = open(DEMO_DEV_NAME, O_RDWR);
+	int fd = open(DEMO_DEV_NAME, O_RDWR | O_NONBLOCK);
 	if (fd < 0) {
 		printf("open devicei %s failed\n", DEMO_DEV_NAME);
 		return -1;
 	}
+
+	int ret = read(fd, read_buffer, 2*len);
+	printf("read %d bytes, buffer=%s\n", ret, read_buffer);
+
+	ret = write(fd, message, len);
+	if (ret != len) {
+		printf("have write %d bytes\n", ret);
+	}
+
+	ret = write(fd, message, len);
+	if (ret != len) {
+		printf("have write %d bytes\n", ret);
+	}
+
+	//close(fd);
+	
+	//fd = open(DEMO_DEV_NAME, O_RDWR);
+	//if (fd < 0) {
+		//printf("open devicei %s failed\n", DEMO_DEV_NAME);
+		//return -1;
+	//}
 
 	ret = read(fd, read_buffer, 2*len);
 	printf("read %d bytes, buffer=%s\n", ret, read_buffer);
